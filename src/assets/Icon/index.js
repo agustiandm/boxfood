c6640f45c590c03d8556f72c37e106347c8f01d8
Agustian Dwi Mulyono <agustiandm@gmail.com>
2020-12-15 08:42:49 +0700

    Create Bottom Tab Navigator

--- src/assets/Icon/index.js
+++ src/assets/Icon/index.js
@@ -1,3 +1,17 @@
 import IcBack from './ic_back.svg';
+import IcHome from './ic_home.svg';
+import IcHomeOff from './ic_home_normal.svg';
+import IcOrder from './ic_order.svg';
+import IcOrderOff from './ic_order_normal.svg';
+import IcProfile from './ic_profile.svg';
+import IcProfileOff from './ic_profile_normal.svg';
 
-export { IcBack };
+export {
+    IcBack,
+    IcHome,
+    IcHomeOff,
+    IcOrder,
+    IcOrderOff,
+    IcProfile,
+    IcProfileOff
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SignIn, SignUp, SplashScreen, SignUpAddress, SignUpSuccess } from '../pages';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SignIn, SignUp, SplashScreen, SignUpAddress, SignUpSuccess, Home, Order, Profile } from '../pages';
+import { BottomNavigator } from '../components';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+    return (
+        <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Order" component={Order} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    )
+};
 
 const Router = () => {
     return (
@@ -34,6 +47,12 @@ const Router = () => {
             <Stack.Screen
                 name="SignUpSuccess"
                 component={SignUpSuccess}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="MainApp"
+                component={MainApp}
                 options={{ headerShown: false }}
             />
 
